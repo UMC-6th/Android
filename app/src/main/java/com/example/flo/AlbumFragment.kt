0d821be660ce5b80bf2df3822465c38251e6261b
seongh06 <seongh06@naver.com>
2024-05-07 01:27:58 +0900

    feat 루카스 3week

--- app/src/main/java/com/example/flo/AlbumFragment.kt
+++ app/src/main/java/com/example/flo/AlbumFragment.kt
@@ -7,10 +7,13 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.flo.databinding.FragmentAlbumBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class AlbumFragment :Fragment(){
+
     lateinit var binding : FragmentAlbumBinding
 
+    private val information = arrayListOf("수록곡", "상세정보", "영상")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -31,6 +34,13 @@ class AlbumFragment :Fragment(){
                 .addToBackStack(null)
                 .commit()
         }
+
+        val albumAdapter = AlbumVPAdapter(this)
+        binding.albumContentVp.adapter = albumAdapter
+        TabLayoutMediator(binding.albumContentTb, binding.albumContentVp) {
+            tab, position ->
+            tab.text = information[position]
+        }.attach()
 
         return binding.root
     }
